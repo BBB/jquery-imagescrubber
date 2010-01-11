fdddc7062838f7068182fce932d359e6a027ad2e
Ollie Relph <ollie@relph.me>
2010-01-11 22:55:27 +0000

    adding js files

---
+++ example/js/page.js
@@ -0,0 +1,12 @@
+$(function() {
+	$('ul.imageskipper.skip-x').imageskipper({
+	  	width: 500,
+	  	height : 332,
+	  	axis : 'x'	
+	});
+	$('ul.imageskipper.skip-y').imageskipper({
+	  	width: 500,
+	  	height : 332,
+	  	axis : 'y'
+	});
+});
