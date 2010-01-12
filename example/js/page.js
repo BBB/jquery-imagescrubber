caffc2208505b55189e692bee8a8e1eaa537384d
Ollie Relph <ollie@relph.me>
2010-01-12 19:52:17 +0000

    Fixing a few styling issues

--- example/js/page.js
+++ example/js/page.js
@@ -2,11 +2,13 @@ $(function() {
 	$('ul.imagescrubber.skip-x').imagescrubber({
 	  	width: 500,
 	  	height : 332,
+		items : 'li',
 	  	axis : 'x'	
 	});
 	$('ul.imagescrubber.skip-y').imagescrubber({
 	  	width: 500,
 	  	height : 332,
+		items : 'li',
 	  	axis : 'y'
 	});
 });
